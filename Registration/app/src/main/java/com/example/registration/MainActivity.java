1e1bcab901b42d9f2c2073905d03fe0e2206b4ca
Jian <duranjhonmark2@gmail.com>
2025-03-06 22:06:01 +0800

    Add Registration App

---
+++ Registration/app/src/main/java/com/example/registration/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.registration;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText firstName;
+    public static final String EXTRA_MESSAGE="com.example.registration.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        firstName =  findViewById(R.id.first_name);
+        Button btnNext = findViewById(R.id.btn_next);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               register();
+            }
+        });
+    }
+
+    public void register()
+    {
+        Intent intent = new Intent(this,MainActivity2.class);
+        String fName = firstName.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,fName);
+        startActivity(intent);
+    }
+}
