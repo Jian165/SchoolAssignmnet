1487450f96b8f8cb9fc5b6e5bad6f0cc5752110d
Jian <duranjhonmark2@gmail.com>
2025-03-21 07:03:42 +0800

    Adding Calculator App

---
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText value;
+    Button delete;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        value = findViewById(R.id.value);
+        delete = findViewById(R.id.btn_Delete);
+
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                value.setText(String.valueOf(0));
+            }
+        });
+
+
+
+
+    }
+}
